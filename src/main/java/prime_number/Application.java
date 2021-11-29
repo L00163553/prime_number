73647076c407fc80d0e15b716d2e02da4dbfd749
L00163553 <L00163553@student.lyit.ie>
2021-11-29 19:09:25 +0000

    added dependency

--- src/main/java/prime_number/Application.java
+++ src/main/java/prime_number/Application.java
@@ -2,13 +2,14 @@ package prime_number;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
