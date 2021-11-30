d1123da6f990281c8777a610dd6700e67f6b8abe
L00163553 <L00163553@student.lyit.ie>
2021-11-30 09:06:43 +0000

    added new html page

--- src/main/java/prime_number/FirstController.java
+++ src/main/java/prime_number/FirstController.java
@@ -14,23 +14,23 @@ public class FirstController {
 	@PostMapping
 	public String getPrime(@RequestParam("number") String number, ModelMap modelMap) {
 		int i,m=0,flag=0;   
-		
+		String result = " ";
 		int n = Integer.parseInt(number);
 		System.out.println(n);
 		  m=n/2;      
 		  if(n==0||n==1){  
-		   return(n+" is not a prime number");      
+		   result = (n+" is not a prime number");      
 		  }else{  
 		   for(i=2;i<=m;i++){      
 		    if(n%i==0){      
 		        
 		     flag=1;      
-		     return(n+" is not a prime number");       
-		
+		     result = (n+" is not a prime number");       
 		    }      
 		   }      
-		   if(flag==0)  { return(n+" is a prime number"); }  
-		  }//
-		  return " ";  
+		   if(flag==0)  { result = (n+" is a prime number"); }  
+		  }
+		return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+        "<body>\n<h1>" + result + "</h1></body>\n" + "</html>";
 		}    
 	}  
