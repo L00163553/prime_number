70b35feebee585785257dd3c67a780bbdcfa3b09
L00163553 <L00163553@student.lyit.ie>
2021-11-30 09:18:05 +0000

    deleted controller

--- src/main/java/prime_number/Application.java
+++ src/main/java/prime_number/Application.java
@@ -3,7 +3,9 @@ package prime_number;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +17,26 @@ public class Application extends SpringBootServletInitializer{
         SpringApplication.run(Application.class, args);
     }
 	
+	@PostMapping("/prime_number/prime")
+	public String getPrime(@RequestParam("number") String number, ModelMap modelMap) {
+		int i,m=0,flag=0;   
+		String result = " ";
+		int n = Integer.parseInt(number);
+		System.out.println(n);
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   result = (n+" is not a prime number");      
+		  }else{  
+		   for(i=2;i<=m;i++){      
+		    if(n%i==0){      
+		        
+		     flag=1;      
+		     result = (n+" is not a prime number");       
+		    }      
+		   }      
+		   if(flag==0)  { result = (n+" is a prime number"); }  
+		  }
+		return "<html>\n" + "<header><title>Prime Number Checker</title></header>\n" +
+        "<body>\n<h1>" + result + "</h1></body>\n" + "</html>";
+		}    
 }
