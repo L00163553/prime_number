ed7e590259464b793a44156cb83adbd1fc436cf2
L00163553 <L00163553@student.lyit.ie>
2021-11-30 00:34:19 +0000

    fixed url path

--- src/main/java/prime_number/FirstController.java
+++ src/main/java/prime_number/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prime")
+@RequestMapping("/prime_number/prime")
 public class FirstController {
 	
 	@PostMapping
